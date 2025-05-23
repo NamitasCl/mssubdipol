54f669330410bf3feb2ac49e7944793e7070a24b
Enzo Ramirez <namas@Megatron.>
2025-05-22 20:35:24 -0400

    Intento de salvar data de contenedor anterior

--- turnos/src/main/java/cl/investigaciones/turnos/controller/AsignacionFuncionarioController.java
+++ turnos/src/main/java/cl/investigaciones/turnos/controller/AsignacionFuncionarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/turno/asignaciones")
+@RequestMapping("/api/turnos/asignaciones")
 @CrossOrigin("*")
 public class AsignacionFuncionarioController {
 
